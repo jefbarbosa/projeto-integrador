f3b3dbaa2b398a419b3dda6c5995a038c7380696
jeferson <jeferson.barbosa@mercadolivre.com>
2022-05-05 13:14:13 -0300

    feat: add bankslip barcode facilities

--- src/main/java/br/com/meli/projetointegrador/controller/BankSlipController.java
+++ src/main/java/br/com/meli/projetointegrador/controller/BankSlipController.java
@@ -2,14 +2,19 @@ package br.com.meli.projetointegrador.controller;
 
 
 import br.com.meli.projetointegrador.dto.BankSlipResult;
+import br.com.meli.projetointegrador.dto.CartDTO;
 import br.com.meli.projetointegrador.model.BankSlip;
+import br.com.meli.projetointegrador.model.Cart;
 import br.com.meli.projetointegrador.service.BankSlipService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,9 +22,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.xml.transform.TransformerException;
+import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/fresh-products/bank-slip")
@@ -28,15 +36,38 @@ public class BankSlipController {
     @Autowired
     private BankSlipService bankSlipService;
 
+    @GetMapping("/find-purchases-by-customer")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
+    public ResponseEntity<List<CartDTO>> getBankSlip() {
+        List<Cart> carts = bankSlipService.getPurchasedCartsByCustomer();
+        List<CartDTO> cartDTOList = carts.stream().map(CartDTO::basicInfo).collect(Collectors.toList());
+        return new ResponseEntity<>(cartDTOList, HttpStatus.OK);
+    }
+
     @GetMapping("/find-by-order")
     @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<BankSlipResult> getBankSlip(@RequestParam Long orderId) {
         return new ResponseEntity<>(bankSlipService.getBankSlip(orderId), HttpStatus.OK);
     }
 
+//    @GetMapping("/barcode")
+//    public ResponseEntity<BufferedImage> getBarcode(@RequestParam String barcode) throws Exception {
+//        return new ResponseEntity<>(bankSlipService.generateEAN13BarcodeImage(barcode), HttpStatus.OK);
+//    }
+
+    @GetMapping("/barcode")
+    public ResponseEntity<String> getBarcode(@RequestParam String barcode) throws Exception {
+        return new ResponseEntity<>(bankSlipService.generateBarcodeB64String(barcode), HttpStatus.OK);
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
+    }
+
     @GetMapping("/generate")
     @PreAuthorize("hasRole('ROLE_CUSTOMER')")
-    public ResponseEntity<byte[]> getPdf(@RequestParam Long orderId) throws IOException, TransformerException {
+    public ResponseEntity<byte[]> getPdf(@RequestParam Long orderId) throws Exception {
 
 //        BankSlipResult bankSlipResult = bankSlipService.getBankSlip(orderId);
 
--- src/main/java/br/com/meli/projetointegrador/service/BankSlipService.java
+++ src/main/java/br/com/meli/projetointegrador/service/BankSlipService.java
@@ -2,15 +2,18 @@ package br.com.meli.projetointegrador.service;
 
 import br.com.meli.projetointegrador.dto.BankSlipResult;
 import br.com.meli.projetointegrador.dto.BankSlipResultImpl;
-import br.com.meli.projetointegrador.model.BankSlip;
-import br.com.meli.projetointegrador.model.Customer;
-import br.com.meli.projetointegrador.model.OrderStatus;
-import br.com.meli.projetointegrador.model.StatusCode;
+import br.com.meli.projetointegrador.exception.UserHasNoOrder;
+import br.com.meli.projetointegrador.exception.UserHasNoPurchaseException;
+import br.com.meli.projetointegrador.model.*;
 import br.com.meli.projetointegrador.repository.BankSlipRepository;
+import br.com.meli.projetointegrador.repository.CartRepository;
 import br.com.meli.projetointegrador.repository.CustomerRepository;
 import br.com.meli.projetointegrador.security.services.UserDetailsImpl;
 import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
 import lombok.AllArgsConstructor;
+import net.sourceforge.barbecue.Barcode;
+import net.sourceforge.barbecue.BarcodeFactory;
+import net.sourceforge.barbecue.BarcodeImageHandler;
 import org.jsoup.Jsoup;
 import org.jsoup.helper.W3CDom;
 import org.jsoup.nodes.Document;
@@ -19,14 +22,20 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.ImageIO;
 import javax.xml.transform.TransformerException;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.DecimalFormat;
+import java.time.LocalDate;
+import java.util.Base64;
+import java.util.List;
+import java.time.temporal.ChronoUnit;
 
 @Service
 @AllArgsConstructor
@@ -34,12 +43,33 @@ public class BankSlipService {
 
     private static final String HTML = "src/main/resources/bank_slip_template.html";
     private static final String PDF = "src/main/resources/bankslip_tmp.pdf";
+    private static final int SLIP_FINAL_PART_LENGTH = 10;
+    private static final Font BARCODE_TEXT_FONT = new Font(Font.SANS_SERIF, Font.PLAIN, 14);
 
     private BankSlipRepository bankSlipRepository;
     private CustomerRepository customerRepository;
+    private CartRepository cartRepository;
 
+
+    public List<Cart> getPurchasedCartsByCustomer() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl userDetails = (UserDetailsImpl) auth.getPrincipal();
+        Long userId = userDetails.getId();
+
+        Customer customer = customerRepository.findCustomerByUser_Id(userId).orElse(new Customer());
+
+        List<Cart> carts = cartRepository.getAllByCustomerIdAndOrderStatus_StatusCode(customer.getId(), StatusCode.PURCHASE);
+
+        if (carts.size() < 1) {
+            throw new UserHasNoPurchaseException("This User has no Purchases!");
+        }
+        return carts;
+    }
 
     public BankSlipResult getBankSlip(Long orderId) {
+
+        getPurchasedCartsByCustomer();
+
         BankSlip bankSlip;
         bankSlip = bankSlipRepository.findByOrderStatus_Id(orderId).orElse(new BankSlip());
         if (bankSlip.getId() != null) {
@@ -65,30 +95,105 @@ public class BankSlipService {
             bankSlip.setOrderStatus(new OrderStatus(orderId, StatusCode.PURCHASE));
 
             bankSlipRepository.save(bankSlip);
+        } else {
+            throw new UserHasNoOrder("User has no Order with this Id!");
         }
 
         return  bankSlipResult;
     }
 
-    public byte[] generateBankSlipPdf(Long orderId) throws IOException, TransformerException {
+    public byte[] generateBankSlipPdf(Long orderId) throws Exception {
         BankSlipResult bankSlipResult = getBankSlip(orderId);
         return generateHtmlToPdf(bankSlipResult);
     }
 
-    public byte[] generateHtmlToPdf(BankSlipResult bankSlipResult) throws IOException, TransformerException {
+    public byte[] generateHtmlToPdf(BankSlipResult bankSlipResult) throws Exception {
         File inputHTML = new File(HTML);
         Document doc = createWellFormedHtml(inputHTML, bankSlipResult);
         return xhtmlToPdf(doc, PDF);
     }
 
-    private Document createWellFormedHtml(File inputHTML, BankSlipResult bankSlipResult) throws IOException {
-        Document doc = Jsoup.parse(inputHTML, "UTF-8");
-        Element link = doc.select("p#interestRate").first();
-        System.out.println(link.text());
-        link.appendText("JJ");
-        link.text("1234.55");
+    private void replaceHtmlInputs(Document doc, String identifier, String value) {
+        //doc.select("p#interestRate").attr("text", "11");
+        Element el = doc.select(identifier).first();
+        if (el != null)
+            el.text(value);
+    }
 
-        doc.select("p#interestRate").attr("text", "11");
+    private void replaceHtmlElemAttr(Element el, String value, String attribute) {
+        String srcValue = el.attr(attribute);
+        el.attr("src", value);
+    }
+
+    private String createBankSlipCode(String initialValue, String date, Double value) {
+        LocalDate processingDate = LocalDate.parse(date);
+        LocalDate baseDate = LocalDate.of(1997, 7, 10);
+        long daysElapsed = ChronoUnit.DAYS.between(baseDate, processingDate);
+        String daysELapsedStr = String.valueOf(daysElapsed);
+
+        DecimalFormat decimalFormat = new DecimalFormat("#.00");
+        String textBankSlipValue = decimalFormat.format(value).replace(".", "");
+
+        String formattedValue = String.format("%1$" + SLIP_FINAL_PART_LENGTH + "s", textBankSlipValue)
+                .replace(' ', '0');
+
+        return initialValue + " " + daysELapsedStr + formattedValue;
+
+    }
+
+//    public String createBarcodeB64(String originalCodeB64, String barcode) {
+//
+//    }
+
+//    public BufferedImage generateEAN13BarcodeImage(String barcodeText) throws Exception {
+//        Barcode barcode = BarcodeFactory.createCode128(barcodeText);
+//        barcode.setFont(BARCODE_TEXT_FONT);
+////        barcode.toString();
+//        BufferedImage bufImg = BarcodeImageHandler.getImage(barcode);
+//
+//        Base64.getEncoder().encodeToString(barcode.toString().getBytes(StandardCharsets.UTF_8));
+//
+//        return BarcodeImageHandler.getImage(barcode);
+//    }
+
+    public String generateBarcodeB64String(String barcodeText) throws Exception {
+        Barcode barcode = BarcodeFactory.createCode128(barcodeText);
+        barcode.setFont(BARCODE_TEXT_FONT);
+        BufferedImage bufImg = BarcodeImageHandler.getImage(barcode);
+
+        final ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+        try (final OutputStream b64os = Base64.getEncoder().wrap(os)) {
+            ImageIO.write(bufImg, "png", b64os);
+        } catch (final IOException ioe) {
+            throw new UncheckedIOException(ioe);
+        }
+
+        return os.toString();
+    }
+
+    private Document createWellFormedHtml(File inputHTML, BankSlipResult bankSlipResult) throws Exception {
+        Document doc = Jsoup.parse(inputHTML, "UTF-8");
+        doc.outputSettings().syntax(Document.OutputSettings.Syntax.xml);
+
+        if (bankSlipResult == null) {
+            return doc;
+        }
+
+        Element el = doc.select("span#bankSlipCode").first();
+        String bankSlipCode = el.text();
+        bankSlipCode = createBankSlipCode(bankSlipCode, bankSlipResult.getDate(), bankSlipResult.getTotal());
+
+        el = doc.select("img#barcodeImg").first();
+        String barcodeB64 = generateBarcodeB64String(bankSlipCode);
+        replaceHtmlElemAttr(el, "data:image/png;base64," + barcodeB64, "src");
+
+        replaceHtmlInputs(doc, "span#bankSlipCode", bankSlipCode);
+        replaceHtmlInputs(doc, "p#totalValueDocument", bankSlipResult.getTotal().toString());
+        replaceHtmlInputs(doc, "div#buyerName", bankSlipResult.getName());
+        replaceHtmlInputs(doc, "div#buyerCpfCnpj", bankSlipResult.getCpf());
+        replaceHtmlInputs(doc, "div#buyerEmail", bankSlipResult.getEmail());
+
         doc.outputSettings()
                 .syntax(Document.OutputSettings.Syntax.xml);
         return doc;
